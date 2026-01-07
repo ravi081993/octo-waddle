d674e157d4c60331073806ecefe2a6c377a81649
rohit <ryrohit.k@gmail.com>
2026-01-07 14:53:27 +0530

    first commit

---
+++ src/main/java/com/rytech/GitWorkFlowTestApplication.java
@@ -0,0 +1,13 @@
+package com.rytech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitWorkFlowTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitWorkFlowTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rytech/GitWorkFlowTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rytech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitWorkFlowTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
